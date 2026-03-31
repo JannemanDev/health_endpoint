a8c886b44dcb5a0d9e1ae3370dad6320afea2a02
Jan Oonk <j.oonk@fontys.nl>
2026-03-31 15:12:58 +0200

    -added helper run script

---
+++ runAsScreen.sh
@@ -0,0 +1,5 @@
+# use --non-interactive so it can be run under a screen unattended
+screen -d -m -S ip_change /volume1/homes/janoonk/health_endpoint/start_health_server.sh
+
+# list all screens: screen -ls
+# restore a screen: screen -r <screen-id/name>
