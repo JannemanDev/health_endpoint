43f73400601ac6dc1f13cdbcea9e591b1e9a6224
Jan Oonk <j.oonk@fontys.nl>
2026-03-31 09:17:43 +0200

    -small change

--- rebuild.windows.bat
+++ rebuild.windows.bat
@@ -3,6 +3,6 @@ docker rm health_server
 
 docker build -t health_server:latest .
 
-docker run -d --name health_server -p 9000:8000 --read-only --tmpfs /tmp --cap-drop ALL health_server:latest
+docker run -d --name health_server --restart unless-stopped -p 9000:8000 --read-only --tmpfs /tmp --cap-drop ALL health_server:latest
 
 docker logs health_server
